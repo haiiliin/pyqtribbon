3f7b99f18cf8fa8070685194cfc8348b577230b1
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-07-22 22:10:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['PyQt5', 'numPy'],
+    install_requires=['PyQt5', 'numpy'],
 )
