4d84b1e28bb8cf14b03b1c7dba42f89c66d57400
Hailin-Wang <2011133@tongji.edu.cn>
2022-07-24 17:30:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires='>=3.7',
     classifiers=[
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
