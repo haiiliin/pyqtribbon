71e8e8a6b77729e0cf67cf0ed0daf2f507e521ea
Hailin Wang <2011133@tongji.edu.cn>
2022-07-27 01:43:35 +0800

    Update to v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyqtribbon",
-    version="0.2.0",
+    version="0.2.1",
     author="WANG Hailin",
     author_email="hailin.wang@connect.polyu.hk",
     description="PyQtRibbon is a Qt-based application framework for building user interfaces.",
