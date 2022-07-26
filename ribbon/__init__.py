086995fc7bae75a62821d7c8b089cf2f618a94e8
Hailin-Wang <2011133@tongji.edu.cn>
2022-07-26 15:03:13 +0800

    Remove unnecessary imports in ribbon.__init__.py

--- ribbon/__init__.py
+++ ribbon/__init__.py
@@ -1,11 +1,4 @@
-from .category import RibbonCategory, RibbonCategoryStyle, RibbonNormalCategory, RibbonContextCategory
-from .categorylayoutwidget import (RibbonCategoryLayoutWidget, RibbonCategoryLayoutButton, RibbonCategoryScrollArea,
-                                   RibbonCategoryScrollAreaContents)
-from .gallery import RibbonGallery, RibbonGalleryListWidget, RibbonGalleryButton, RibbonGalleryPopupListWidget
-from .panel import RibbonPanel, RibbonPanelTitle, RibbonSpaceFindMode, RibbonGridLayoutManager, RibbonPanelItemWidget
+from .category import RibbonCategoryStyle
 from .panel import RibbonSpaceFindMode
 from .ribbonbar import RibbonBar, RibbonStyle
-from .separator import RibbonSeparator, RibbonHorizontalSeparator, RibbonVerticalSeparator
-from .tabbar import RibbonTabBar
-from .titlewidget import RibbonTitleWidget, RibbonApplicationButton, RibbonTitleLabel
-from .toolbutton import RibbonToolButton, RibbonButtonStyle
+from .toolbutton import RibbonButtonStyle
