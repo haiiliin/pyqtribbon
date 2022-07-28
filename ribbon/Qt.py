b63ae0531caae620b52db4b43491784b5508f115
Hailin Wang <2011133@tongji.edu.cn>
2022-07-28 18:15:00 +0800

    Update Qt.py

--- ribbon/Qt.py
+++ ribbon/Qt.py
@@ -1,14 +1,10 @@
 import sys
-import typing
 
 from qtpy import QtWidgets, QtGui
 
-qApp = None  # type: typing.Optional[QtWidgets.QApplication]
-
 
 def mkQApp(name=None):
-    global qApp
-    qApp = QtWidgets.QApplication.instance() or QtWidgets.QApplication(sys.argv or ["pyqtribbon"])
+    qApp = QtWidgets.QApplication.instance() or QtWidgets.QApplication(sys.argv)
     qApp.setFont(QtGui.QFont("Times New Roman", 8))
 
     if name is not None:
