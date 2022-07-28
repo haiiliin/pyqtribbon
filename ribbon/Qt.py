bf5613634b77586a4e513d8becf14860e65cf4b5
Hailin Wang <2011133@tongji.edu.cn>
2022-07-28 17:20:50 +0800

    Add mkQApp function

---
+++ ribbon/Qt.py
@@ -0,0 +1,21 @@
+import sys
+import typing
+
+from qtpy import QtWidgets, QtGui
+
+qApp = None  # type: typing.Optional[QtWidgets.QApplication]
+
+
+def mkQApp(name=None):
+    global qApp
+    qApp = QtWidgets.QApplication.instance() or QtWidgets.QApplication(sys.argv)
+    qApp.setFont(QtGui.QFont("Times New Roman", 8))
+
+    if name is not None:
+        qApp.setApplicationName(name)
+    return qApp
+
+
+def exec_():
+    app = mkQApp()
+    return app.exec() if hasattr(app, 'exec') else app.exec_()
