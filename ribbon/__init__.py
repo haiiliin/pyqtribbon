1ebbae278e0a132137f411c547384bbdd277ad43
Hailin-Wang <2011133@tongji.edu.cn>
2022-07-28 14:48:08 +0800

    Add __version__ in __init__.py

--- ribbon/__init__.py
+++ ribbon/__init__.py
@@ -3,3 +3,5 @@ from .panel import RibbonSpaceFindMode, ColumnWise, RowWise
 from .ribbonbar import RibbonBar, RibbonStyle, Default, Debug
 from .toolbutton import RibbonButtonStyle, Small, Medium, Large
 from .menu import RibbonMenu, RibbonPermanentMenu
+
+_version__ = "0.3.0"
