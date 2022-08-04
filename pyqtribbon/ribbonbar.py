9cb62cddd12d7f5efa215dd1ab810b027fc86040
Hailin Wang <2011133@tongji.edu.cn>
2022-08-05 01:41:09 +0800

    Update ribbonbar.py

--- pyqtribbon/ribbonbar.py
+++ pyqtribbon/ribbonbar.py
@@ -53,7 +53,7 @@ class RibbonBar(QtWidgets.QMenuBar):
     _ribbonVisible = True
 
     #: heights of the ribbon elements
-    _ribbonHeight = 240
+    _ribbonHeight = 200
 
     #: current tab index
     _currentTabIndex = 0
