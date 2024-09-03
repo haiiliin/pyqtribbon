b0e9046e8bc2b99c52fa1402b9c522ed5681c4d9
Hailin Wang <wanghailin@tongji.edu.cn>
2024-09-03 22:34:10 +0800

    Update version configurations

--- pyqtribbon/version.py
+++ pyqtribbon/version.py
@@ -1,31 +1,21 @@
 from pathlib import Path
-
-try:
-    from ._version import version as default_version
-except ImportError:
-    default_version = "0.0.0-unknown"
+from re import error as RegexError
 
 
 def _get_version():
-    """Return the version string used for __version__."""
-    # Only shell out to a git subprocess if really needed, and not on a
-    # shallow clone, such as those used by CI, as the latter would trigger
-    # a warning from setuptools_scm.
     root = Path(__file__).resolve().parents[1]
     if (root / ".git").exists() and not (root / ".git/shallow").exists():
-        # noinspection PyBroadException
         try:
             import setuptools_scm
 
-            return setuptools_scm.get_version(
-                root=str(root),
-                version_scheme="post-release",
-                fallback_version=default_version,
-            )
-        except Exception:
-            return default_version
-    else:  # Get the version from the _version.py setuptools_scm file.
-        return default_version
+            return setuptools_scm.get_version(root=str(root))
+        except (ImportError, RegexError, LookupError):
+            try:
+                from ._version import version
+
+                return version
+            except ImportError:
+                return "0.0.0-unknown"
 
 
 __version__ = _get_version()
